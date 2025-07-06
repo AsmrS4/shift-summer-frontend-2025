ff84c191abe4b7f029774d8f32d0475e5b2c6cd9
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-07-06 22:46:52 +0700

    added input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Input.scss';
+
+interface InputProps {
+    label?: string;
+    type?: string;
+    placeholder?: string;
+    value: string | number | null;
+    error?: boolean;
+    errorMessage?: string;
+    hint?: string;
+    hintMessage?: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const Input: React.FC<InputProps> = ({
+    label,
+    value,
+    onChange,
+    placeholder,
+    type = 'text',
+    error,
+    hint,
+    errorMessage,
+    hintMessage,
+}) => {
+    return (
+        <>
+            {label && <label className='input__label'>{label}</label>}
+            <input
+                className='input'
+                type={type}
+                placeholder={placeholder}
+                onChange={onChange}
+                value={value}
+            />
+            {hint && !error && <p className={'input__hint'}>{hintMessage}</p>}
+            {error && <p className={'input__hint error'}>{errorMessage}</p>}
+        </>
+    );
+};
+
+export default Input;
