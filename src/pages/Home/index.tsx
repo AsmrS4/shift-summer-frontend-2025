d01f7e898e7771d7d301fee0a9e4cc75eb9eebf4
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-07-06 22:47:20 +0700

    finished main page layout(desktop version)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PlaceIcon from '@mui/icons-material/Place';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import './Home.scss';
 import { Box } from '@mui/material';
-import box from '@assets/package-open.svg';
-import qr from '@assets/QR_koronapay.svg';
-import bgImage from '@assets/bg-image.svg';
+
 import ActionButton from '@components/Button';
 import Select from '@components/Select';
 import { useAppSelector } from '@hooks/useAppSelector';
@@ -14,14 +12,24 @@ import { useDispatch } from 'react-redux';
 import { fetchAddress } from '@store/Address/AddressCreator';
 import { fetchPackageParams } from '@store/Packages/PackageCreator';
 
+import box from '@assets/package-open.svg';
+import qr from '@assets/QR_koronapay.svg';
+import bgImage from '@assets/bg-image.svg';
+import bgImageSmall from '@assets/s_empty96-s.svg';
+import Input from '@components/Input';
+
 const HomePage = () => {
     const { cities } = useAppSelector((state) => state.addressReducer);
     const { packages } = useAppSelector((state) => state.packageReducer);
+    const [orderNumber, setOrderNumber] = useState<number | null>(null);
     const dispatch: any = useDispatch();
     useEffect(() => {
         dispatch(fetchAddress());
         dispatch(fetchPackageParams());
     }, []);
+    const handleOrderSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setOrderNumber(e.target.valueAsNumber);
+    };
     return (
         <main className='home-page'>
             <div className='container'>
@@ -66,11 +74,26 @@ const HomePage = () => {
                             <Box className={'box tracker'}>
                                 <h2 className='box__title'>Отследить посылку</h2>
                                 <div className='wrapper'>
-                                    <input type='text' />
+                                    <Input
+                                        value={orderNumber}
+                                        onChange={handleOrderSearch}
+                                        type='number'
+                                        placeholder='Номер заказа'
+                                    />
                                     <ActionButton text={'Найти'} type={'button'} disabled={false} />
                                 </div>
                             </Box>
-                            <Box className={'box advertise'}></Box>
+                            <Box className={'box advertise'}>
+                                <div className='inner-wrapper'>
+                                    <div className='text'>
+                                        <h2>
+                                            <b>Бесплатная доставка</b>
+                                        </h2>
+                                        <p>за приведенного друга</p>
+                                    </div>
+                                    <img src={bgImageSmall} alt='' />
+                                </div>
+                            </Box>
                         </div>
                     </div>
                 </section>
