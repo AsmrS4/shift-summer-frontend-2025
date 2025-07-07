e39b450a5faac1a15cab8189f93b0e99c361e6d4
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-07-07 14:40:26 +0700

    extended link classes

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,15 +1,20 @@
 import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
 import './Link.scss';
 
 interface NavProps {
     href: string;
     text: string;
     icon?: ReactNode;
+    isSelected?: boolean;
 }
 
-const NavLink = ({ href, text, icon }: NavProps) => {
+const NavLink = ({ href, text, icon, isSelected = false }: NavProps) => {
+    const linkClassName = clsx('link', {
+        active: isSelected,
+    });
     return (
-        <a href={href} className='link'>
+        <a href={href} className={linkClassName}>
             {icon}
             <p>{text}</p>
         </a>
